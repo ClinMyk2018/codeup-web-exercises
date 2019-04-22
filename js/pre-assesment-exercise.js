59aab9f385d48659e2b3aa5f36c9a3716beaed61
Mykal Clinard <mykalclinard@Mykals-MacBook-Pro.local>
2019-04-22 08:57:09 -0500

    morning

--- js/pre-assesment-exercise.js
+++ js/pre-assesment-exercise.js
@@ -16,14 +16,94 @@
 // ```
 
 
+var input = ["fred", true, 5, 3, 12];
+
 function filteredNumbers(arr){
     var output=[];
 
+    for (var i=0; i<arr.length; i +=1){
+        //verify if element is a number
+        //if element is number, push onto output arr
+        if(typeof arr[i] === "number"){
+            output.push(arr[i]);
+        }
+    output.sort(function (a, b) {
+        if (a > b) {
+            return 1;
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    }
+
 }
 
+console.log(filteredNumbers(input));
 
 
 
+
+//
+// ## Problem 2
+//
+// Write a function, ```getOlder```, that takes in an array of dog objects and increases the value of the age properties by 1. No return value is needed since the original array of objects will be changed by this function.
+//
+// Example Input:
+//
+//     ```js
+var dogs = [
+    {
+        name: "Chompers",
+        breed: "Pug",
+        age: 7
+    },
+    {
+        name: "Freddy",
+        breed: "Lab",
+        age: 4
+    },
+    {
+        name: "Mr. Pig",
+        breed: "Mastif",
+        age: 10
+    }
+];
+// ```
+//
+// Example Output:
+//
+//     ```js
+// [
+//     {
+//         name: "Chompers",
+//         breed: "Pug",
+//         age: 8
+//     },
+//     {
+//         name: "Freddy",
+//         breed: "Lab",
+//         age: 5
+//     },
+//     {
+//         name: "Mr. Pig",
+//         breed: "Mastif",
+//         age: 11
+//     }
+// ];
+// ```
+
+
+
+
+function getOlder(dogs){
+dogs.forEach(function(dogs){
+    dogs.age +=1;
+});
+}
+getOlder(dogs);
+console.log(dogs);
 
 
 // var letters = ["a", "b", "c"];
@@ -54,19 +134,19 @@ function filteredNumbers(arr){
 //     Example Input:
 //
 //     ```js
-var breads  = [
-    "white",
-    "wheat",
-    "rhy",
-    "white"
-];
-
-var fillings = [
-    "pb&j",
-    "ham",
-    "cheese steak",
-    "tuna"
-];
+// var breads  = [
+//     "white",
+//     "wheat",
+//     "rhy",
+//     "white"
+// ];
+//
+// var fillings = [
+//     "pb&j",
+//     "ham",
+//     "cheese steak",
+//     "tuna"
+// ];
 //
 // makeSandwhichObjects(breads, fillings) // example call to the function
 // ```
@@ -93,22 +173,22 @@ var fillings = [
 //     }
 // ]
 // ```
-
-function makeSandwichObjects(bread, fillings){
-    var output = [];
-    for (var i=0; i<breads.length; i += 1){
-
-        var sandwich ={};
-        sandwich.bread = breads[i];
-        sandwich.fillings = fillings[i];
-
-        // or
-        //
-        // output.push({
-        // breads: bread[i],
-        //     fillings: fillings[i]
-        // });
-    }
-    return output;
-}
-console.log(makeSandwichObjects(breads, fillings));
+//
+// function makeSandwichObjects(breads, fillings){
+//     var output = [];
+//     for (var i=0; i<breads.length; i += 1){
+//
+//         var sandwich ={};
+//         sandwich.breads = breads[i];
+//         sandwich.fillings = fillings[i];
+//
+//         // or
+//         //
+//         // output.push({
+//         // breads: breads[i],
+//         //     fillings: fillings[i]
+//         // });
+//     }
+//     return output;
+// }
+// console.log(makeSandwichObjects(breads, fillings));
